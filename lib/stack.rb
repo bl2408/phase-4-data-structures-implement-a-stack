6a506bf6526f86fad2f7afbc8f53e1e38a5c5b94
Brian Lambert <brianlambert2408@gmail.com>
2023-01-06 11:25:11 +1100

    commit tree 4998243b9102dde79fb2adc3cec051a760ec6cd1

--- lib/stack.rb
+++ lib/stack.rb
@@ -2,12 +2,16 @@
 class Stack
  
 
-    def initialize
+    def initialize limit = nil
         @stack = []
+        @limit = limit
     end
 
     def push value
-        @stack.push value
+        raise 'Stack Overflow' if full?
+
+        @stack.push(value)
+
     end
 
     def pop
@@ -17,6 +21,25 @@ class Stack
 
     def peek
         @stack[-1]
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit && @stack.length == @limit
+    end
+
+    def search target
+        @stack.each_with_index do |item, index|
+            return @stack.length - index - 1 if item == target
+        end
+        return -1
     end
 
 end
